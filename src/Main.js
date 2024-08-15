8f30108a4818fb79f1d3a2fab1653e7e2142da05
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-08-15 16:17:06 +0530

    updated

--- src/Main.js
+++ src/Main.js
@@ -26,12 +26,25 @@ function Main() {
             {backgroundColor === "white" ? "White Theme" : "Black Theme"}
           </button>
 
-          <h1>
-            “In any war, there are calms between storms. There will be days when
-            we lose faith. Days when our allies turn against us...but the day
-            will never come that we forsake this planet and its people.” ―
-            Optimus Prime
-          </h1>
+          <h1 className="title">Optimus Prime {`>`} Quotes</h1>
+          <div className="content">
+            <h1>
+              “In any war, there are calms between storms. There will be days
+              when we lose faith. Days when our allies turn against us...but the
+              day will never come that we forsake this planet and its people.” ―
+              Optimus Prime
+            </h1>
+            <h1>
+              “Freedom is the right of all sentient beings.” ― Optimus Prime
+            </h1>
+            <h1>"We must have courage. We can't ignore the danger. We must conquer it.” ― Optimus Prime</h1>
+            <h1>
+              “There's a thine line between being a hero and being a memory.” ―
+              Optimus Prime
+            </h1>
+            <h1>"I am Optimus Prime, leader of the autobots.” ―
+            Optimus Prime</h1>
+          </div>
         </div>
       </section>
     </>
